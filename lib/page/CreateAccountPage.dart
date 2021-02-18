5b4a10668d151f19b4a6662f8d4be173b691b5f4
Ida Bagus Putu Peradnya Dinata <peradnya@gmail.com>
2021-02-18 20:01:41 +0800

    create the welcome page.

--- lib/page/CreateAccountPage.dart
+++ lib/page/CreateAccountPage.dart
@@ -1,4 +1,5 @@
 import 'package:create_account/component/step_indicator/StepIndicator.dart';
+import 'package:create_account/page/WelcomePage.dart';
 import 'package:flutter/material.dart';
 
 class CreateAccountPage extends StatefulWidget {
@@ -8,51 +9,132 @@ class CreateAccountPage extends StatefulWidget {
 
 class CreateAccountPageState extends State<CreateAccountPage> {
   int _steps;
+  TextEditingController _emailTextController;
+  GlobalKey<FormState> _welcomeForm,
+      _createPasswordForm,
+      _personalInfoForm,
+      _scheduleVideoCallForm;
+  FocusScopeNode _focus;
 
   @override
   void initState() {
-    _steps = 1;
+    _steps = 0;
+    _welcomeForm = GlobalKey<FormState>();
+    _createPasswordForm = GlobalKey<FormState>();
+    _personalInfoForm = GlobalKey<FormState>();
+    _scheduleVideoCallForm = GlobalKey<FormState>();
+
+    _emailTextController = TextEditingController();
     super.initState();
   }
 
-  Widget buildWelcomeStep() {
-    return Container();
+  @override
+  void didChangeDependencies() {
+    _focus = FocusScope.of(context);
+    super.didChangeDependencies();
   }
 
-  Widget buildCreatePasswordStep() {
-    return Container();
-  }
-
-  Widget buildPersonalInfoStep() {
-    return Container();
-  }
-
-  Widget buildScheduleVideoCallStep() {
-    return Container();
+  void _disableSoftKeyboard() {
+    if (!_focus.hasPrimaryFocus && _focus.focusedChild != null) {
+      _focus.focusedChild.unfocus();
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: _steps == 0
-          ? null
-          : AppBar(
-              elevation: 0,
-              leading: IconButton(
-                icon: const Icon(Icons.arrow_back),
-                onPressed: () {},
+    Widget page;
+    switch (_steps) {
+      case 0:
+        page = WelcomePage(
+          welcomeForm: _welcomeForm,
+          emailTextController: _emailTextController,
+        );
+        break;
+      case 1:
+        page = Container();
+        break;
+      case 2:
+        page = Container();
+        break;
+      case 3:
+        page = Container();
+        break;
+      default:
+        page = Container();
+        break;
+    }
+
+    return Listener(
+      onPointerDown: (_) => _disableSoftKeyboard(),
+      child: Scaffold(
+        appBar: _steps == 0
+            ? AppBar(
+                elevation: 0,
+                title: Container(),
+              )
+            : AppBar(
+                elevation: 0,
+                leading: IconButton(
+                  icon: const Icon(Icons.arrow_back),
+                  onPressed: () {
+                    setState(() {
+                      if (_steps > 0) {
+                        _steps = _steps - 1;
+                      }
+                    });
+                  },
+                ),
+                title: Text('Create Account'),
               ),
-              title: Text('Create Account'),
-            ),
-      body: Container(
-        color: Colors.blue,
-        child: Column(
-          children: [
-            StepIndicator(
-              currentStep: 3,
-              maxStep: 4,
-            )
-          ],
+        body: Container(
+          color: Colors.blue,
+          child: Column(
+            children: [
+              StepIndicator(
+                currentStep: _steps,
+                maxStep: 4,
+              ),
+              Expanded(
+                child: Container(
+                  color: _steps == 0 ? Colors.white : Colors.blue,
+                  child: SingleChildScrollView(
+                    child: page,
+                  ),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.all(20),
+                width: double.infinity,
+                color: _steps == 0 ? Colors.white : Colors.blue,
+                child: RaisedButton(
+                  color: Colors.lightBlue,
+                  child: Text(
+                    'Next',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  onPressed: () {
+                    switch (_steps) {
+                      case 0:
+                        if (_welcomeForm.currentState.validate()) {
+                          setState(() {
+                            _steps = _steps + 1;
+                          });
+                        } else {}
+                        break;
+                      case 1:
+                        break;
+                      case 2:
+                        break;
+                      case 3:
+                        break;
+                      default:
+                        break;
+                    }
+                  },
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
---
+++ lib/page/WelcomePage.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+class WelcomePage extends StatelessWidget {
+  final GlobalKey<FormState> welcomeForm;
+  final TextEditingController emailTextController;
+
+  const WelcomePage({
+    Key key,
+    @required this.welcomeForm,
+    @required this.emailTextController,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: welcomeForm,
+      child: Container(
+        width: double.infinity,
+        color: Colors.white,
+        padding: EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            RichText(
+              text: TextSpan(
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold),
+                children: [TextSpan(text: 'Welcome to')],
+              ),
+            ),
+            RichText(
+              text: TextSpan(
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold),
+                children: [
+                  TextSpan(text: 'GIN '),
+                  TextSpan(
+                      text: 'Finans', style: TextStyle(color: Colors.blue)),
+                ],
+              ),
+            ),
+            Container(height: 20),
+            Text(
+              'Welcome to The Bank of The Future. Manage and track your accounts on the go.',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 14),
+            ),
+            Container(height: 20),
+            TextFormField(
+              decoration: InputDecoration(
+                icon: Icon(Icons.email_outlined),
+                labelText: 'Email',
+              ),
+              keyboardType: TextInputType.emailAddress,
+              controller: emailTextController,
+              validator: (value) {
+                if (value.isEmpty) {
+                  return 'Email must not empty';
+                }
+                return null;
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
